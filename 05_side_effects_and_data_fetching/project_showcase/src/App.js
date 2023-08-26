a7fd29a053916a42fd4732ec582f8021a211e797
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-08-25 21:11:15 -0400

    completed lecture

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,43 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
-// Deliverable 1: Implement useEffect in App component 
+import Timer from "./components/Timer"
+
+// Deliverable 1: Implement useEffect in App component
 // to load projects
 
   // Import the `useEffect` hook from the React library
 
-  // Invoke `useEffect` and make a `GET` request using 
+  // Invoke `useEffect` and make a `GET` request using
   // the `fetch` method
 
-  // Update `projects` state upon successful response 
+  // Update `projects` state upon successful response
   // from the server
 
-// Deliverable 2: Demonstrate the order of operations 
-// between rendering a component and running the side 
+// Deliverable 2: Demonstrate the order of operations
+// between rendering a component and running the side
 // effect
 
-  // Place a console.log() inside the `App` component as 
+  // Place a console.log() inside the `App` component as
   // well as the `useEffect` method
 
-  // Open up the devtools to observe when each phase of 
-  // the component will occur 
+  // Open up the devtools to observe when each phase of
+  // the component will occur
 
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isTimerVisible, setIsTimerVisible] = useState(false)
 
   const onAddProject = newProject => {
     const newProjectCollection = [...projects, newProject];
     setProjects(newProjectCollection);
   }
 
-  const handleClick = () => {
+  useEffect(() => {
     fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
-      .then((projects) => setProjects(projects));
-  };
+    .then((res) => res.json())
+    .then((projects) => setProjects(projects))
+  }, [])
+
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
@@ -45,15 +49,21 @@ const App = () => {
   // With POST Request
   const onError = filteredList => { setProjects(filteredList) };
 
+  const handleClick = () => {
+    setIsTimerVisible(!isTimerVisible)
+  }
+
   return (
     <div className={isDarkMode ? "App" : "App light"}>
+      <button onClick={handleClick}>Toggle Timer</button>
+      {isTimerVisible ? <Timer /> : null}
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm 
-        onAddProject={onAddProject} 
+      <ProjectForm
+        onAddProject={onAddProject}
         onError={onError}
         projects={projects}
       />
-      <button onClick={handleClick}>Load Projects</button>
+      {/* <button onClick={handleClick}>Load Projects</button> */}
       <ProjectList projects={projects} />
     </div>
   );
--- 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
@@ -1,8 +1,28 @@
-// Deliverable 3: Demonstrate the unmounting and cleanup 
+// Deliverable 3: Demonstrate the unmounting and cleanup
 // phase of a component through `useEffect`
 
-    // Return a cleanup function inside the `useEffect` 
+    // Return a cleanup function inside the `useEffect`
     // with a console.log()
 
-    // Open up the devtools to observe when the cleanup 
+    // Open up the devtools to observe when the cleanup
     // will occur during the stages of Component Lifecycle
+import { useState, useEffect } from "react"
+const Timer = () => {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCount(prevCount => prevCount + 1)
+        }, 1000)
+        
+        return () => clearInterval(intervalId)
+    },[] )
+
+    return(
+        <>
+        <h1>Timer:</h1>
+        <p>Count:{count}</p>
+        </>
+
+    )
+}
+export default Timer
